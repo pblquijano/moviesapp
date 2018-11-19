cc0bc400a2b742ec262599a5243a5e261fad3138
pblquijano <pablo.quijano@dacodes.com.mx>
2018-11-19 17:14:38 -0600

    Precompile assets

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
+describe Movie do
+    it "is valid with a firstname, lastname and email" do
+      movie = Movie.new(
+        title: 'The Movie',
+        genre: 'Thriller',
+        directed_by: 'Pablo Quijano',
+        price: 99,
+        synopsis: 'This is the synopsis.',
+        duration: 120)
+      expect(movie).to be_valid
+    end
+  
+    it "is invalid without a title" do
+      movie = Movie.new(title: nil)
+      movie.valid?
+      expect(movie.errors[:title]).to include("can't be blank")
+    end
+  
+    it "is invalid without a genre" do
+      movie = Movie.new(genre: nil)
+      movie.valid?
+      expect(movie.errors[:genre]).to include("can't be blank")
+    end
+  
+    it "is invalid without a directed_by" do
+        movie = Movie.new(directed_by: nil)
+        movie.valid?
+        expect(movie.errors[:genre]).to include("can't be blank")
+    end
 
-RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "is invalid without a price" do
+        movie = Movie.new(price: nil)
+        movie.valid?
+        expect(movie.errors[:price]).to include("can't be blank")
+    end
+
+    it "is invalid without a synopsis" do
+      movie = Movie.new(synopsis: nil)
+      movie.valid?
+      expect(movie.errors[:synopsis]).to include("can't be blank")
+    end
+
+    it "is invalid without a duration" do
+        movie = Movie.new(duration: nil)
+        movie.valid?
+        expect(movie.errors[:duration]).to include("can't be blank")
+      end
 end
