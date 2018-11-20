fbd3561bcd7e7e68f6a5fc251682a7ceeb2bb0d4
pblquijano <pablo.quijano@dacodes.com.mx>
2018-11-19 18:16:05 -0600

    Config fonts deploy

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 module Moviesapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.paths << Rails.root.join("app","assets","fonts")
     config.load_defaults 5.1
-    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
